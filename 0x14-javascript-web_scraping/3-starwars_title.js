0befdb85198806dcb34eedd2e52f2e69192bc85b
AbaraEmmanuel <emmanuelabara265@gmail.com>
2024-03-26 05:37:11 -0700

    JAVASCRIPT

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
 const request = require('request');
-const movieId = process.argv[2];
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-
-request.get(url, (error, response, body) => {
-	  if (error) {
-		      console.log(error);
-		    } else {
-			        const data = JSON.parse(body);
-			        console.log(data.title);
-			      }
+const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+	  console.log(error || JSON.parse(body).title);
 });
